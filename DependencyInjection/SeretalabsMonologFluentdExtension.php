73530e015dc68ac972a5b5d11420dd66571f16a0
alex <alex@cobrowser.net>
2014-09-09 09:56:52 +0200

    fixed typo

--- DependencyInjection/SeretalabsMonologFluentdExtension.php
+++ DependencyInjection/SeretalabsMonologFluentdExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Seretalabs\Bundle\MonoloFluentdBundle\DependencyInjection;
+namespace Seretalabs\Bundle\MonologFluentdBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
